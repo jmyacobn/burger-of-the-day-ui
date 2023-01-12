99c6d1cd22d91ffd2522c4210f04928df942e709
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-12 13:36:16 -0700

    Add error to API call

--- src/Components/APIcalls/APIcalls.js
+++ src/Components/APIcalls/APIcalls.js
@@ -2,7 +2,7 @@ export const getRecipes = () => {
   return fetch('http://localhost:3001/api/v1/recipes')
     .then(response => {
       if(!response.ok) {
-        throw new Error()
+        throw new Error(`${response.status}`)
       }
       return response.json()
     })
