43e20545f04c83d92c8e3d014ef0f2c4d7b26d32
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-12 13:01:43 -0700

    Add styling to the recipe container and cards

---
+++ src/Components/APIcalls/APIcalls.js
@@ -0,0 +1,9 @@
+export const importRecipes = () => {
+  return fetch('http://localhost:3001/api/v1/recipes')
+    .then(response => {
+      if(!response.ok) {
+        throw new Error()
+      }
+      return response.json()
+    })
+}
--- src/Components/RecipeCard/RecipeCard.js
+++ src/Components/RecipeCard/RecipeCard.js
@@ -4,8 +4,15 @@ import './RecipeCard.css';
 const RecipeCard = ( { id, name, price} ) => {
   return (
     <div className='recipe-card' key={id}>
-      <h2>{name}</h2>
-      <p>{price}</p>
+      <div className='label'>
+      <h2>Burger of the Day</h2>
+      </div>
+      <div className='name'>
+        <h2>{name}</h2>
+      </div>
+      <div className='price'>
+        <p>{price}</p>
+      </div>
     </div>
   )
 }
