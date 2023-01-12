d9157227620f0bc356f70309d8a32d0be0d7d942
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-12 08:00:00 -0700

    Set up app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,23 @@
-
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recipes: [
+
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Burger of the Day</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
