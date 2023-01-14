110c33d84a3de5b8898e43ce49d3dcffeb0506ee
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-14 08:09:43 -0700

    Add nav link to Bob's Burgers logo

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from '../../../src/logo.png'
-import '../NavBar/NavBar.css'
+import logo from '../../../src/logo.png';
+import { NavLink } from 'react-router-dom';
+import '../NavBar/NavBar.css';
 
 const NavBar = () => {
   return (
     <nav>
-      <img className='logo' src={logo} alt='Bobs Burgers Logo'/>
+      <NavLink to='/' activeClassName='active'>
+        <img className='logo' src={logo} alt='Bobs Burgers Logo'/>
+      </NavLink>
     </nav>
+   
   )
 }
 
