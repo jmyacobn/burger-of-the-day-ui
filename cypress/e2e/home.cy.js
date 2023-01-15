f482e19f395f3c8a66efd7b1858c1c0509fc5c9e
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-15 10:05:13 -0700

    Add recipes fixture

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Page Load Flows', () => {
+  beforeEach(() => {
+    cy.intercept('http://localhost:3001/api/v1/recipes', {
+      method: 'GET',
+      fixture: '../fixtures/recipes.json'
+    })
+    cy.visit('http://localhost:3000')
   })
+
 })
