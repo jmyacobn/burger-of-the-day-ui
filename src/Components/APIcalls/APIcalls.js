61e0ca37a90e7cbc758a627475fd8f642a8cbb9e
Jenn Yacoubian <jyacoubian@gmail.com>
2023-01-22 16:33:28 -0700

    Change URL for API call

--- src/Components/APIcalls/APIcalls.js
+++ src/Components/APIcalls/APIcalls.js
@@ -1,5 +1,5 @@
 export const getRecipes = () => {
-  return fetch('http://localhost:3001/api/v1/recipes')
+  return fetch('https://burger-of-the-day-api.vercel.app/api/v1/recipes')
     .then(response => {
       if(!response.ok) {
         throw new Error(`${response.status}`)
